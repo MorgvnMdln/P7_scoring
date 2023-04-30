3d9452ccc2b45ed59d340eeca81a9f1820e864a0
Morgvn Mdln <morgvn.mdln@gmail.com>
2023-04-30 13:47:51 +0200

    Show the folders of app

--- prediction_model.py
+++ prediction_model.py
@@ -37,8 +37,8 @@ CURRENT_DIRECTORY = os.path.dirname(os.path.realpath(__file__))
 
 def list_dir():
     for root, dirs, files in os.walk(CURRENT_DIRECTORY):
-        for file in files:
-            print(file)
+        for dir in dirs: # for file in files:
+            print(dir)  # file
     # CURRENT_DIRECTORY = os.path.abspath(os.path.join(os.path.dirname(__file__), os.path.pardir))
     # Path(__file__).resolve().parent.parent
     # os.path.abspath(os.path.join(os.path.dirname(__file__), os.path.pardir))
@@ -77,7 +77,7 @@ class PredictionModel:
         # self.test_data = load_pickle(self.TEST_DATA_PATH)
         # if not os.path.exists(self.TEST_DATA_PATH):
         # load_from_google_cloud('1ABIbSgQrQWn07UkJoDcEsl9vT1rbz2lk', self.TEST_DATA_PATH)
-        # list_dir()  # CURRENT_DIRECTORY
+        list_dir()  # CURRENT_DIRECTORY
         # print(CURRENT_DIRECTORY)
         # print(TEST_DATA_PATH)
         self.test_data = pd.read_json(self.TEST_DATA_PATH)
