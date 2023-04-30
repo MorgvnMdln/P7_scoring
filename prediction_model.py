984961eac27eb0d305abcb9716deed7dd709a4c2
Morgvn Mdln <morgvn.mdln@gmail.com>
2023-04-30 13:15:31 +0200

    Search of TEST_DATA_PATH in prediction_model.py

--- prediction_model.py
+++ prediction_model.py
@@ -77,8 +77,9 @@ class PredictionModel:
         # self.test_data = load_pickle(self.TEST_DATA_PATH)
         # if not os.path.exists(self.TEST_DATA_PATH):
         # load_from_google_cloud('1ABIbSgQrQWn07UkJoDcEsl9vT1rbz2lk', self.TEST_DATA_PATH)
-        # list_dir()
+        list_dir(CURRENT_DIRECTORY)
         print(CURRENT_DIRECTORY)
+        print(TEST_DATA_PATH)
         self.test_data = pd.read_json(self.TEST_DATA_PATH)
         
         # self.load_feat_importance_local()  # self.local_feat_importance = self.load_feat_importance_local()
